87e75e80533fab0c52317259e5a46fcf2ca126be
SpeedMentor <82273949+SpeedMentor@users.noreply.github.com>
2025-02-02 21:01:31 +0300

    Update HelloWorld.vue

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="hello">
-    <h1>Hello World!</h1>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      default: 'Hello, Vue.js!',
+    },
+  },
 };
 </script>
 
 <style scoped>
 .hello {
+  text-align: center;
   margin-top: 20px;
 }
 </style>
