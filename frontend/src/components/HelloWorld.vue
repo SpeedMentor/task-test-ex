2dbbdd7b75fcc6bb60cb33b784d0363e777608f2
SpeedMentor <82273949+SpeedMentor@users.noreply.github.com>
2025-02-02 04:27:16 +0300

    Create HelloWorld.vue

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
+
+<style scoped>
+.hello {
+  margin-top: 20px;
+}
+</style>
