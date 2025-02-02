4fa318849b3ede901cb4f97dfef0c5ddf4826df6
SpeedMentor <82273949+SpeedMentor@users.noreply.github.com>
2025-02-02 20:45:42 +0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
-
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'mydb',
 });
-
+db.connect((err) => {
+  if (err) throw err;
+  console.log('MySQL connected...');
+});
 module.exports = db;
