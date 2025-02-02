50504396e8faedb0ea90644997ea0cd42c60b789
SpeedMentor <82273949+SpeedMentor@users.noreply.github.com>
2025-02-02 20:46:11 +0300

    Update dataModel.js

--- backend/src/models/dataModel.js
+++ backend/src/models/dataModel.js
@@ -1,1 +1,15 @@
-test
+const db = require('../config/db');
+
+const DataModel = {
+  getAllData: async () => {
+    try {
+      const query = 'SELECT * FROM sample_data';
+      const [rows] = await db.promise().query(query);
+      return rows;
+    } catch (error) {
+      throw new Error(`Error fetching data: ${error.message}`);
+    }
+  },
+};
+
+module.exports = DataModel;
