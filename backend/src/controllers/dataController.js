abac82a14ad2610eed4be241253a403079b07477
SpeedMentor <82273949+SpeedMentor@users.noreply.github.com>
2025-02-02 20:46:36 +0300

    Update dataController.js

--- backend/src/controllers/dataController.js
+++ backend/src/controllers/dataController.js
@@ -1,28 +1,10 @@
-const db = require('../config/db');
-const redisClient = require('redis').createClient();
+const DataModel = require('../models/dataModel');
 
-exports.getData = (req, res) => {
-  const cacheKey = 'data';
-
-  // Check Redis cache
-  redisClient.get(cacheKey, async (err, data) => {
-    if (err) throw err;
-
-    if (data) {
-      // Return cached data
-      res.json(JSON.parse(data));
-    } else {
-      // Fetch data from MySQL
-      const query = 'SELECT * FROM sample_data';
-      db.query(query, (err, results) => {
-        if (err) throw err;
-
-        // Cache data in Redis
-        redisClient.setex(cacheKey, 3600, JSON.stringify(results));
-
-        // Return data
-        res.json(results);
-      });
-    }
-  });
+exports.getAllData = async (req, res) => {
+  try {
+    const data = await DataModel.getAllData();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
