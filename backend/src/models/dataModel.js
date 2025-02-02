9427d4585914bb1f73533417a340c3a09166dcb1
SpeedMentor <82273949+SpeedMentor@users.noreply.github.com>
2025-02-02 21:14:46 +0300

    Update dataModel.js

--- backend/src/models/dataModel.js
+++ backend/src/models/dataModel.js
@@ -1,10 +1,21 @@
 const db = require('../config/db');
+const redisClient = require('../config/redis');
 
 const DataModel = {
   getAllData: async () => {
     try {
-      const query = 'SELECT * FROM sample_data';
-      const [rows] = await db.promise().query(query);
+      // Check if data is cached in Redis
+      const cacheData = await redisClient.get('sample_data');
+      if (cacheData) {
+        return JSON.parse(cacheData);
+      }
+
+      // Fetch data from MySQL if not cached
+      const [rows] = await db.query('SELECT * FROM sample_data');
+
+      // Store data in Redis with a 1-hour expiration time
+      await redisClient.setEx('sample_data', 3600, JSON.stringify(rows));
+
       return rows;
     } catch (error) {
       throw new Error(`Error fetching data: ${error.message}`);
