7ffce3284938f79f2b4027c546e49d604e686c5f
SpeedMentor <82273949+SpeedMentor@users.noreply.github.com>
2025-02-02 21:15:47 +0300

    Update dataRoutes.js

--- backend/src/routes/dataRoutes.js
+++ backend/src/routes/dataRoutes.js
@@ -3,6 +3,7 @@ const dataController = require('../controllers/dataController');
 
 const router = express.Router();
 
+// Route to fetch all data
 router.get('/', dataController.getAllData);
 
 module.exports = router;
