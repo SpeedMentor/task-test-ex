0d6f1a2dbbce8d281c10e0588664ecd70ffa6e8a
SpeedMentor <82273949+SpeedMentor@users.noreply.github.com>
2025-02-04 21:14:31 +0300

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import './assets/main.css'
 
-Vue.config.productionTip = false;
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './stores/store'
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+
+app.mount('#app')
