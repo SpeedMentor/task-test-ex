46431bd494c1e0dcb18c497073479135a66478e9
SpeedMentor <82273949+SpeedMentor@users.noreply.github.com>
2025-02-05 03:01:11 +0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const dataRoutes = require('./src/routes/dataRoutes');
+const dataRoutes = require('./src/routes/dataRoutes.js');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
